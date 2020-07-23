eaa4d088bbb7d4d1a8ad0ee915e0ba2129bb6843
Roger <1584@holbertonschool.com>
2020-07-23 14:45:37 -0500

    task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+  * binary_tree_uncle - uncle node find
+  * @node: node find uncle
+  * Return: node NULL or not
+  */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !(node->parent) || !(node->parent->parent))
+		return (NULL);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
+}
